48ecc9717c87e3ff2847bba0681c352de0b28460
元凛 <xrkffgg@gmail.com>
2021-06-16 00:38:16 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,21 +1,66 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { exec } = require('@actions/exec');
 const { Octokit } = require('@octokit/rest');
+
+const token = core.getInput('token');
+const octokit = new Octokit({ auth: `token ${token}` });
+const context = github.context;
+const { dealStringToArr } = require('actions-util');
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    if (context.eventName !== 'pull_request_target') {
+      const { owner, repo } = context.repo;
+      core.info(`owner: ${owner}, repo: ${repo}`);
+      const cis = core.getInput('ci').split('\n');
+      try {
+        for (const ci of cis) {
+          core.info(`[Run] ${ci}`);
+          await exec(ci);
+        }
+        core.setOutput('result', 'success');
+      } catch (err) {
+        const noticeType = core.getInput('notice-type');
+        const noticeTitle = core.getInput('notice-title') || `# 🤖 ${owner}/${repo} CI Notice`;
+        const noticeBody =
+          core.getInput('notice-body') ||
+          'CI: \n\n```' + `\n${ci}\n` + '```' + `\n\n🚨 Operation failed, please check in time!`;
 
-    core.debug(new Date().toTimeString());
-    await new Promise(resolve => {
-      setTimeout(() => resolve('done!'), 10);
-    });
-    core.debug(new Date().toTimeString());
+        if (noticeType === 'dingding') {
+          const dingdingToken = core.getInput('dingding-token');
+          axios.post(`https://oapi.dingtalk.com/robot/send?access_token=${dingdingToken}`, {
+            msgtype: 'markdown',
+            markdown: {
+              title: noticeTitle,
+              text: `${noticeTitle}\n\n${noticeBody}`,
+            },
+          });
+        }
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+        if (noticeType === 'issue') {
+          const labels = core.getInput('issue-labels');
+          const assignees = core.getInput('issue-assignees');
+
+          const params = {
+            owner,
+            repo,
+            title: noticeTitle,
+            body: noticeBody,
+            labels: dealStringToArr(labels),
+            assignees: dealStringToArr(assignees),
+          };
+          const { data } = await octokit.issues.create(params);
+          core.info(`Actions: [create-issue][${data.number}] success!`);
+        }
+
+        core.setOutput('result', 'failed');
+      }
+    } else {
+      core.setFailed(`This Action is not support "pull_request_target"!`);
+    }
+  } catch (err) {
+    core.setFailed(err.message);
   }
 }
 
